736fcbb9f8e0abd03c3ddc7f1149bf468c976b5e
BgAlex <bgalex2003@gmail.com>
2024-12-14 23:23:23 -0500

    Progress

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import pandas as pd
 import plotly.express as px
 from io import StringIO
 import base64
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
@@ -34,7 +41,7 @@ app.layout = html.Div([
 def parse_file_input(contents):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
-    return pd.read_csv(StringIO(decoded.decode('utf-8')))
+    return pd.read_csv(StringIO(decoded.decode('utf-8')),index_col=0)
 
 @app.callback(
     [Output('file-upload-feedback', 'children'),
@@ -101,6 +108,20 @@ def update_based_on_file(file_contents, file_name):
                 dcc.Graph(id='bar-chart', style={'margin-top': '20px'})
             ], style={'width': '48%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '10px'})
 
+            training_section = html.Div(
+                [
+                    dcc.Checklist(options=df.columns, value=[], id='training_features_selector', inline=True),
+                    html.Br(),
+                    html.Button('Train Model', id='start_training', n_clicks=0),
+                    html.Div(id='r2-score')
+                ],
+                style={
+                        'display': 'block',
+                        'justify-content': 'center',
+                        'margin-bottom': '20px'
+                    },
+            )
+
             correlation_section = html.Div([
                 html.H4(id='correlation-title', style={'text-align': 'center', 'margin-top': '10px'}),  # set dynamically via update_correlations_title()
                 dcc.Graph(id='correlation-chart', style={'margin-top': '20px'})
@@ -108,12 +129,37 @@ def update_based_on_file(file_contents, file_name):
 
             return (
                 "",
-                html.Div([target_dropdown, html.Div([analysis_section, correlation_section])]),
+                html.Div([target_dropdown, html.Div([analysis_section, correlation_section]), training_section]),
                 df.to_dict('records')
             )
         except Exception as e:
             return f"Error processing file: {str(e)}", "", None
     return "", "", None
+
+@app.callback(
+    Output('r2-score', 'children'),
+    [
+    Input('start-training','n_clicks'),
+    Input('training-features-selector', 'value'),
+    Input('target-selector', 'value'),
+    Input('stored-data', 'data'),],
+    prevent_initial_call=True
+)
+def train_model(n_clicks, selected_categories, selected_target, data):
+    if data:
+        df = pd.DataFrame(data)
+        pipe = Pipeline([
+            ('imputer', SimpleImputer(strategy='constant')),
+            ('encoder', OneHotEncoder(handle_unknown='ignore')), # when encountering an unseen category, the encoder will assign it all zeros
+            ('model', RandomForestRegressor(random_state=42))
+        ])
+        x_train, x_test, y_train, y_test = train_test_split(df[selected_categories], df[selected_target], test_size=0.2, random_state=42)
+        pipe.fit(x_train, y_train)
+        y_pred = pipe.predict(y_train)
+        r2 = r2_score(y_test, y_pred)
+        return f"R2 Score: {r2}"
+    return "Could not conlude processing."
+
 
 @app.callback(
     Output('graph-title', 'children'),
